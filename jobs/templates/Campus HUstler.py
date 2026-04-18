a1badbe4ba7898a099e25d4f85b1dd74cbf22fa4
adriana0alvarez <avallad3@students.kennesaw.edu>
2026-04-17 22:32:48 -0400

    Create Campus HUstler.py

---
+++ jobs/templates/Campus HUstler.py
@@ -0,0 +1,7 @@
+echo "# Campus-Hustler" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ksu-is/Campus-Hustler.git
+git push -u origin main
